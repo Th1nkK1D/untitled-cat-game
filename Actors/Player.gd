5dd0b72e7484c3671e4760dd4a025910c0440031
panangam <panangam@gmail.com>
2020-09-24 00:55:24 -0700

    rename rotate funcs to turn

--- Actors/Player.gd
+++ Actors/Player.gd
@@ -7,7 +7,6 @@ export var player_position = Vector2()
 const CELLSIZE = 2
 
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass
@@ -17,9 +16,9 @@ func _input(event):
 	if event.is_action_pressed("ui_up"):
 		move_forward()
 	elif event.is_action_pressed("ui_right"):
-		rotate_right()
+		turn_right()
 	elif event.is_action_pressed("ui_left"):
-		rotate_left()
+		turn_left()
 	update_visual()
 	print(player_direction, player_position)
 	
@@ -40,8 +39,8 @@ func move_forward():
 		offset.x = 1
 	player_position += offset
 	
-func rotate_left():
+func turn_left():
 	player_direction = (player_direction + 1) % 4
 	
-func rotate_right():
+func turn_right():
 	player_direction = posmod(player_direction - 1, 4)

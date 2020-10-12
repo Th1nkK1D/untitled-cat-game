d9695a5ce72f3ee33c45098c75a59ba3d849c22c
Th1nkK1D <witheep@gmail.com>
2020-10-12 20:48:59 +0700

    Add cat position and rotation tweening

--- Actors/Cat.gd
+++ Actors/Cat.gd
@@ -1,31 +1,49 @@
 extends KinematicBody
 
-
 enum Directions {NORTH, WEST, SOUTH, EAST}
-export(Directions) var player_direction
-export var player_position = Vector2()
 const CELLSIZE = 2
 
+signal arrived(player_position)
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass
+export(Directions) var player_direction
+export var player_position = Vector2()
+export var move_speed = 5
+export var turn_speed = 5
 
+var is_moving = false
+
+onready var target_position = translation
+onready var target_rotation = rotation
 
 func _input(event):
 	if event.is_action_pressed("ui_up"):
-		move_forward()
+		if !is_moving:
+			move_forward()
+			is_moving = true
 	elif event.is_action_pressed("ui_right"):
 		turn_right()
 	elif event.is_action_pressed("ui_left"):
 		turn_left()
+		
 	update_visual()
 	print(player_direction, player_position)
 	
+func _process(delta):
+	if !translation.is_equal_approx(target_position):
+		translation = translation.move_toward(target_position, move_speed * delta)
+		
+		if translation.is_equal_approx(target_position):
+			is_moving = false
+			emit_signal("arrived", player_position)
+		
+	if !rotation.is_equal_approx(target_rotation):
+		rotation = rotation.move_toward(target_rotation, turn_speed * delta)
+		
+	
 func update_visual():
-	rotation.y = player_direction * PI / 2
-	translation.x = -player_position.x * CELLSIZE
-	translation.z = player_position.y * CELLSIZE
+	target_rotation.y = player_direction * PI / 2
+	target_position = Vector3(-player_position.x, 0, player_position.y) * CELLSIZE
+
 	
 func move_forward():
 	var offset = Vector2()

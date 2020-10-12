ba7f471ed45a731062fd98c59056acaf1d689b94
Th1nkK1D <witheep@gmail.com>
2020-10-12 23:55:30 +0700

    Fix cat turn direction and remove player scene

--- Actors/Cat.gd
+++ Actors/Cat.gd
@@ -10,22 +10,25 @@ export var player_position = Vector2()
 export var move_speed = 5
 export var turn_speed = 5
 
-var is_moving = false
+var is_transforming = false
 
 onready var target_position = translation
 onready var target_rotation = rotation
 
 func _input(event):
+	if is_transforming:
+		return;
+		
 	if event.is_action_pressed("ui_up"):
-		if !is_moving:
-			move_forward()
-			is_moving = true
+		move_forward()
+		is_transforming = true
 	elif event.is_action_pressed("ui_right"):
 		turn_right()
+		is_transforming = true
 	elif event.is_action_pressed("ui_left"):
 		turn_left()
-		
-	update_visual()
+		is_transforming = true
+	
 	print(player_direction, player_position)
 	
 func _process(delta):
@@ -33,20 +36,21 @@ func _process(delta):
 		translation = translation.move_toward(target_position, move_speed * delta)
 		
 		if translation.is_equal_approx(target_position):
-			is_moving = false
+			translation = target_position
+			is_transforming = false
 			emit_signal("arrived", player_position)
 		
 	if !rotation.is_equal_approx(target_rotation):
 		rotation = rotation.move_toward(target_rotation, turn_speed * delta)
 		
-	
-func update_visual():
-	target_rotation.y = player_direction * PI / 2
-	target_position = Vector3(-player_position.x, 0, player_position.y) * CELLSIZE
-
-	
+		if rotation.is_equal_approx(target_rotation):
+			target_rotation.y = player_direction * PI / 2
+			rotation.y = target_rotation.y
+			is_transforming = false
+		
 func move_forward():
 	var offset = Vector2()
+	
 	if player_direction == Directions.NORTH:
 		offset.y = 1
 	elif player_direction == Directions.WEST:
@@ -55,13 +59,17 @@ func move_forward():
 		offset.y = -1
 	elif player_direction == Directions.EAST:
 		offset.x = 1
+		
 	player_position += offset
+	target_position = Vector3(-player_position.x, 0, player_position.y) * CELLSIZE
 	
 func turn_left():
 	player_direction = (player_direction + 1) % 4
+	target_rotation.y = rotation.y + PI / 2
 	
 func turn_right():
 	player_direction = posmod(player_direction - 1, 4)
+	target_rotation.y = rotation.y - PI / 2
 
 func clone():
 	var new_cat = duplicate()
